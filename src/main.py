96837bd736b4bc4b48911d1b7e058a55f942626e
Antoine-Girard <antoine.girard@epitech.eu>
2024-12-03 16:43:17 +0100

    ADD: fully working web-scanner skeleton, detection of SQL injection feature working

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from utils import process_url
 if __name__ == "__main__":
     ac = len(sys.argv)
     av = sys.argv
-    if ac < 2 or ac > 2:
+    if ac < 2 or ac > 2 or av[1] == "-h" or av[1] == "--help":
         print("Usage: python main.py <url>")
         sys.exit(1)
     url = av[1]
--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,12 @@ import json
 import os
 from bs4 import BeautifulSoup
 from pathlib import Path
+from urllib.parse import urlparse
+
+JSONNAME = "./output.json"
 
 def process_url(url):
-    clear_json("./output.json")
+    clear_json()
     if url.startswith("https://") != True:
         url = "https://" + url
 
@@ -20,7 +23,10 @@ def process_url(url):
         print("Failed to retrieve the page")
         return 1
 
+    set_json(url)
     vuln_list = []
+    for function_check in one_time_function_list:
+        vuln_list = function_check(vuln_list, url)
     HTMLsoup = BeautifulSoup(response.text, 'html.parser')
     forms = HTMLsoup.find_all('form')
     for form in forms:
@@ -38,47 +44,72 @@ def process_url(url):
 ## Functions scanning for vulnerabilities to check
 #############################################################################
 
+def check_parameters(url):
+    parsed_url = urlparse(url)
+    return bool(parsed_url.query)
+
+def check_url_sql(vuln_list, url):
+    if check_parameters(url):
+        add_entry_to_json("SQL injection", "URL", "")
+        vuln_list.append(["SQL injection", "URL contains parameters"])
+    return vuln_list
 
 def check_SQL(vuln_list, url, name, type):
     check_list_type_sql = ["email", "password", "search", "text"]
-    check_list_name_sql = ["username", "password", "id", "query", "search"]
     if type in check_list_type_sql:
-        add_entry_to_json("./output.json", "SQL injection", "form type : " + type)
+        add_entry_to_json("SQL injection", "form type : " + type, "form name : " + name)
         vuln_list.append(["SQL injection", "form type : " + type])
-        return vuln_list
-    elif name in check_list_name_sql:
-        add_entry_to_json("./output.json", "SQL injection", "form name : " + name)
-        vuln_list.append(["SQL injection", "form name : " + name])
         return vuln_list
     return vuln_list
 
-def check_password(vuln_list, url, name, type):
+def check_brute_force(vuln_list, url, name, type):
     return vuln_list
 
 ## add scanning fuctions in the list to execute them
-function_check_list = [check_SQL, check_password]
-
+function_check_list = [check_SQL, check_brute_force]
+one_time_function_list = [check_url_sql]
 
 #############################################################################
 ## Writing entries in json
 #############################################################################
 
-
-def add_entry_to_json(file_path, name, found_in):
-    if Path(file_path).is_file():
-        with open(file_path, 'r') as file:
+def set_json(url):
+    if Path(JSONNAME).is_file():
+        with open(JSONNAME, 'r') as file:
             data = json.load(file)
     else:
         data = []
 
-    new_entry = {
-        "name": name,
-        "found_in": found_in
+    start_entry = {
+        "URL scanned": url
     }
+    data.append(start_entry)
+    with open(JSONNAME, 'w') as file:
+        json.dump(data, file, indent=4)
+    file.close()
+
+# if the entry doesn't have a name, put "" (or an empty string) for the 'form_name' parameter
+def add_entry_to_json(name, found_in, form_name):
+    if Path(JSONNAME).is_file():
+        with open(JSONNAME, 'r') as file:
+            data = json.load(file)
+
+    if (form_name == ""):
+        new_entry = {
+            "name": name,
+            "found_in": found_in
+        }
+    else:
+        new_entry = {
+            "name": name,
+            "found_in": found_in,
+            "form_name": form_name
+        }
     data.append(new_entry)
 
-    with open(file_path, 'w') as file:
+    with open(JSONNAME, 'w') as file:
         json.dump(data, file, indent=4)
 
-def clear_json(file_path):
-    os.remove(file_path)
+def clear_json():
+    if Path(JSONNAME).is_file():
+        os.remove(JSONNAME)
