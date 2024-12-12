1a7f22cc6037bf6e66c355cd27f433e7dec6c04a
joseph susanyan <joseph@JOSEPH-DESK>
2024-12-12 15:41:50 +0100

    feat: Optimize functions + logger creation

--- src/utils.py
+++ src/utils.py
@@ -4,31 +4,31 @@ from urllib.parse import urlparse
 from password_security import check_asvs_l1_password_security_V2_1_1
 from constants import *
 from json_edit import *
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("scanner.log"), # logs are written in this file
+        logging.StreamHandler()             
+    ]
+)
+logger = logging.getLogger(__name__)
 
-def process_url(url):
-    clear_json()
-    if url.startswith("https://") != True:
-        url = "https://" + url
-
-    print(f"Processing URL: " + url)
+def fetch_page(url):
     try:
         response = requests.get(url)
-    except:
-        print("Failed to retrieve the page")
-        return 1
-    if response.status_code != 200:
-        print("Failed to retrieve the page")
-        return 1
+        if response.status_code != 200:
+            logger.error(f"Failed to retrieve the page. Status code: {response.status_code}")
+            return None
+        logger.info("Page retrieved successfully")
+        return response
+    except requests.RequestException as e:
+        logger.error(f"Error fetching the page: {e}")
+        return None
 
-    set_json(url)
-    vuln_list = []
-    for function_check in one_time_function_list:
-        vuln_list = function_check(vuln_list, url)
-    HTML_soup = BeautifulSoup(response.text, 'html.parser')
-    forms = HTML_soup.find_all('form')
-    global HAS_CAPTCHA
-    HAS_CAPTCHA = check_for_captcha(response, HTML_soup)
+def process_forms(vuln_list, forms, url):
     for form in forms:
         inputs = form.find_all('input')
         for input_field in inputs:
@@ -36,8 +36,35 @@ def process_url(url):
             input_type = input_field.get('type', 'text')
             for function_check in function_check_list:
                 vuln_list = function_check(vuln_list, url, input_name, input_type)
-    print(vuln_list)
+    return vuln_list
+
+
+def process_url(url):
+    clear_json()
+    if not url.startswith("https://"):
+        url = "https://" + url
+
+    logger.info(f"Processing URL: {url}")
+    response = fetch_page(url)
+    if not response:
+        return 1
+
+    set_json(url)
+    vuln_list = []
+
+    for function_check in one_time_function_list:
+        vuln_list = function_check(vuln_list, url)
+
+    # Forms analyze
+    HTML_soup = BeautifulSoup(response.text, 'html.parser')
+    forms = HTML_soup.find_all('form')
+    global HAS_CAPTCHA
+    HAS_CAPTCHA = check_for_captcha(response, HTML_soup)
+    vuln_list = process_forms(vuln_list, forms, url)
+
+    logger.info(f"Vulnerabilities detected: {vuln_list}")
     return 0
+
 
 
 #############################################################################
