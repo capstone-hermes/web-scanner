1b59ca2981eaa24458194f2c523b37f92f1f48e8
joseph susanyan <joseph@JOSEPH-DESK>
2025-02-07 11:42:23 +0100

    feat: Add HEADER const

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,7 @@
 JSONNAME = "./output.json"
 HAS_CAPTCHA = False
+
+# HTTP Headers to pretend to be a navigator
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
